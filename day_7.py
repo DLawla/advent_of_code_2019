9711ca5832cd2bd5c11e723129e38dabaa16620a
Dan Lawler <dan.r.lawler@gmail.com>
2019-12-13 09:30:49 +1300

    working part 2

--- day_7.py
+++ day_7.py
@@ -1,19 +1,18 @@
 program = [3,8,1001,8,10,8,105,1,0,0,21,42,67,88,105,114,195,276,357,438,99999,3,9,101,4,9,9,102,3,9,9,1001,9,2,9,102,4,9,9,4,9,99,3,9,1001,9,4,9,102,4,9,9,101,2,9,9,1002,9,5,9,1001,9,2,9,4,9,99,3,9,1001,9,4,9,1002,9,4,9,101,2,9,9,1002,9,2,9,4,9,99,3,9,101,4,9,9,102,3,9,9,1001,9,5,9,4,9,99,3,9,102,5,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,99]
 
-
-
 class AmplifierIntcode():
     def __init__(self, program, input, phase_setting):
         self.i = 0
-        self.program = program
+        self.program = program.copy()
         self.input = input
         self.phase_setting = phase_setting
         self.phase_input_set = False
         self.halted = False
 
+
     def parameter_value(self, parameter, mode):
         if mode == 0:
-            return int(self.program[parameter])
+            return self.program[parameter]
         else:
             return parameter
 
@@ -60,9 +59,12 @@ class AmplifierIntcode():
 
             elif opcode == 3:  # takes input and saves to parameter address
                 if self.phase_input_set:
-                    self.program[parameter1] = input
+                    if self.input is None:
+                        break
+                    self.program[parameter1] = self.input
+                    self.input = None
                 else:
-                    self.program[parameter1] = phase_setting
+                    self.program[parameter1] = self.phase_setting
                     self.phase_input_set = True
                 self.i += 2
 
@@ -150,36 +152,36 @@ def all_feedback_mode_phase_settings():
     all = [i for i in range(55555, 99999)]
     return [str(i).zfill(5) for i in all if ''.join(sorted(list(str(i).zfill(5)))) == '56789' ]
 
-
-program = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
 max_output = 0
 max_output_phase_settings = None
 for phase_settings in all_feedback_mode_phase_settings():
     output = 0
+    previous_last_output = None
 
-    while True:
-        amplifier_programs = [
-            AmplifierIntcode(program.copy(), 0, int(phase_settings[0])),
-            AmplifierIntcode(program.copy(), 0, int(phase_settings[1])),
-            AmplifierIntcode(program.copy(), 0, int(phase_settings[2])),
-            AmplifierIntcode(program.copy(), 0, int(phase_settings[3])),
-            AmplifierIntcode(program.copy(), 0, int(phase_settings[4])),
-        ]
+    amplifier_programs = [
+        AmplifierIntcode(program.copy(), None, int(phase_settings[0])),
+        AmplifierIntcode(program.copy(), None, int(phase_settings[1])),
+        AmplifierIntcode(program.copy(), None, int(phase_settings[2])),
+        AmplifierIntcode(program.copy(), None, int(phase_settings[3])),
+        AmplifierIntcode(program.copy(), None, int(phase_settings[4])),
+    ]
 
+    in_progress = True
+    while in_progress:
         for i in range(len(phase_settings)):
             amplifier = amplifier_programs[i]
             amplifier.input = output
             output = amplifier.run()
-            print(i)
-            print(amplifier.i)
+
+            if output and i == 4:
+                previous_last_output = output
 
             if amplifier.halted and i == 4:
+                in_progress = False
                 break
 
-    print(output)
-
-    if output > max_output:
-        max_output = output
+    if previous_last_output > max_output:
+        max_output = previous_last_output
         max_output_phase_settings = phase_settings
 
 print('Solution for feedback mode:')
